cd4f381dddb87f2b4de00ea909c20829e70f0a3b
ny-plus-ny <ny.plus.ny@gmail.com>
2023-10-12 10:00:59 +0900

    SUIT 폰트 적용

---
+++ Front-end/src/constants/index.ts
@@ -0,0 +1,8 @@
+
+import fonts from './fonts'
+
+export const MIN_Y = 280; //바텀시트가 최대로 높이 올라갔을 때 y값
+export const MAX_Y = window.innerHeight - 220; //바텀시트가 최소로 내려갔을 때의 y값
+export const BOTTOM_SHEET_HEIGHT = window.innerHeight - MIN_Y; //바텀시트의 세로 길이
+
+export { fonts }
