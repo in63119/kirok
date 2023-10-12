20553d70a43517f046e34254e39cddf3096e7c6e
ny-plus-ny <ny.plus.ny@gmail.com>
2023-10-12 14:39:39 +0900

    HeaderContainer 컴포넌트 추가

---
+++ Front-end/src/components/Header/HeaderContainer.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderContainer = (props) => {
+    const {isGoback, goBackHandler} = props
+    
+  return (
+        <Container>
+            <LeftMenu onClick={goBackHandler} >
+                { isGoback && <GobackIcon src='/images/icon_goBack.png' />}
+            </LeftMenu>
+        </Container>
+  )
+}
+
+export default HeaderContainer
+
+const Container = styled.div`
+    padding: 14px 20px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+`
+
+const LeftMenu = styled.div`
+    cursor: pointer;
+`
+
+const GobackIcon = styled.img`
+    width: 24px;
+    height: 24px;
+`
