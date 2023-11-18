b7e92aee56c4ff94d57ebbdc219429c281ee04e8
이인수 <in63119@gmail.com>
2023-11-18 16:33:12 +0900

    Fix: .env 로컬 & 배포 환경 통합

--- Front-end/src/apis/kakao.ts
+++ Front-end/src/apis/kakao.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const apiKey = process.env.REACT_APP_KAKAO_API_KEY;
-const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+const currentUrl = window.location.host;
+const { REACT_APP_LOCAL_REDIRECT_URI, REACT_APP_PROD_REDIRECT_URI } = process.env;
+const redirect_uri = currentUrl === 'localhost:3000' ? REACT_APP_LOCAL_REDIRECT_URI : REACT_APP_PROD_REDIRECT_URI;
 const kakaoUri = `https://kauth.kakao.com/oauth/authorize?client_id=${apiKey}&redirect_uri=${redirect_uri}&response_type=code`;
 
 const serverURL = process.env.REACT_APP_SERVER_URL;
