2fb8546e37e5d9f1aaea4eb0c366a86e29f48c44
이인수 <in63119@gmail.com>
2023-11-18 16:41:38 +0900

    Fix: redirect_uri 재설정

--- Front-end/src/apis/kakao.ts
+++ Front-end/src/apis/kakao.ts
@@ -3,8 +3,7 @@ axios.defaults.withCredentials = true;
 
 const apiKey = process.env.REACT_APP_KAKAO_API_KEY;
 const currentUrl = window.location.host;
-const { REACT_APP_LOCAL_REDIRECT_URI, REACT_APP_PROD_REDIRECT_URI } = process.env;
-const redirect_uri = currentUrl === 'localhost:3000' ? REACT_APP_LOCAL_REDIRECT_URI : REACT_APP_PROD_REDIRECT_URI;
+const redirect_uri = currentUrl === 'localhost:3000' ? 'http://localhost:3000/callback' : 'https://kirok.net/callback';
 const kakaoUri = `https://kauth.kakao.com/oauth/authorize?client_id=${apiKey}&redirect_uri=${redirect_uri}&response_type=code`;
 
 const serverURL = process.env.REACT_APP_SERVER_URL;
