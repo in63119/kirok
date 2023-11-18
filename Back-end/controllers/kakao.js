6e7cec774cc1239cb8be371574d3bb4c62d68594
이인수 <in63119@gmail.com>
2023-11-18 16:12:06 +0900

    Add: kakao redirectUri 설정

--- Back-end/controllers/kakao.js
+++ Back-end/controllers/kakao.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const axios = require("axios");
 
-const { KAKAO_API_KEY, FRONT_REDIRECT_URI } = process.env;
+const { KAKAO_API_KEY } = process.env;
 const kakaoUrl = "https://kauth.kakao.com";
 
 const { checkUser, addUser } = require("../controllers/kirokDB");
@@ -13,11 +13,16 @@ const { checkUser, addUser } = require("../controllers/kirokDB");
 
 module.exports = {
   kakaoLogin: async (req, res) => {
+    const redirectUri =
+      req.headers.origin === "http://localhost:3000"
+        ? "http://localhost:3000/callback"
+        : "https://kirok.net/callback";
+
     const { code } = req.body;
     const data = {
       grant_type: "authorization_code",
       client_id: KAKAO_API_KEY,
-      redirect_uri: FRONT_REDIRECT_URI,
+      redirect_uri: redirectUri,
       code: code,
     };
     const header = {
@@ -28,48 +33,52 @@ module.exports = {
       .map((k) => encodeURIComponent(k) + "=" + encodeURIComponent(data[k]))
       .join("&");
 
-    const kakaoToken = await axios.post(
-      `${kakaoUrl}/oauth/token`,
-      queryString,
-      { headers: header }
-    );
-    const acceseToken = kakaoToken.data.access_token;
+    try {
+      const kakaoToken = await axios.post(
+        `${kakaoUrl}/oauth/token`,
+        queryString,
+        { headers: header }
+      );
+      const acceseToken = kakaoToken.data.access_token;
 
-    const getUserInfo = async (token) => {
-      header.Authorization += token;
-      const propertyKeys = {
-        property_keys: '["kakao_account.email"]',
+      const getUserInfo = async (token) => {
+        header.Authorization += token;
+        const propertyKeys = {
+          property_keys: '["kakao_account.email"]',
+        };
+
+        const get = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+          headers: header,
+          propertyKeys,
+        });
+        return get;
       };
+      const userInfo = await getUserInfo(acceseToken);
 
-      const get = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
-        headers: header,
-        propertyKeys,
-      });
-      return get;
-    };
-    const userInfo = await getUserInfo(acceseToken);
+      const email = userInfo.data.kakao_account.email;
+      const kakaoId = userInfo.data.id;
 
-    const email = userInfo.data.kakao_account.email;
-    const kakaoId = userInfo.data.id;
+      const isExists = await checkUser(kakaoId);
 
-    const isExists = await checkUser(kakaoId);
-
-    // kirok DB에 있으면
-    if (isExists) {
-      try {
-        res.status(200).send({ email: email, kakaoId: kakaoId });
-      } catch (err) {
-        res.status(400).send(err);
-      }
-      // kirok DB에 없으면
-    } else {
-      const added = await addUser(kakaoId, email);
-
-      if (added) {
-        res.status(200).send({ email: email, kakaoId: kakaoId });
+      // kirok DB에 있으면
+      if (isExists) {
+        try {
+          res.status(200).send({ email: email, kakaoId: kakaoId });
+        } catch (err) {
+          res.status(400).send(err);
+        }
+        // kirok DB에 없으면
       } else {
-        res.status(400).send("DB 추가 중 실패");
+        const added = await addUser(kakaoId, email);
+
+        if (added) {
+          res.status(200).send({ email: email, kakaoId: kakaoId });
+        } else {
+          res.status(400).send("DB 추가 중 실패");
+        }
       }
+    } catch (e) {
+      // console.log(e);
     }
   },
 };
