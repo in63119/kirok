43d9403a53db8345deba6842c74c12561d1b3360
이인수 <in63119@gmail.com>
2023-11-24 18:31:18 +0900

    Add: aws s3 삭제 기능 추가

--- Front-end/src/utils/aws.ts
+++ Front-end/src/utils/aws.ts
@@ -1,6 +1,6 @@
 import * as AWS from 'aws-sdk';
 
-import kirok from './kirok.png'; // test용 지워야 함
+import kirok from './우럭.jpeg'; // test용 지워야 함
 
 const { REACT_APP_ACCESS_KEY, REACT_APP_SECRET_ACCESS_KEY, REACT_APP_REGION } = process.env;
 
@@ -12,7 +12,26 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
+const formData = new FormData();
+formData.append('image', kirok);
+
 export const upload = async () => {
+	const params = {
+		Bucket: 'kirok-user',
+		Key: 'kirok.jpg',
+		Body: kirok,
+		ContentType: 'image/jpeg',
+	};
+
+	s3.upload(params, (err: any, data: any) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`File uploaded successfully. ${data.Location}`);
+	});
+};
+
+export const modify = () => {
 	const params = {
 		Bucket: 'kirok-user',
 		Key: 'kirok.jpg',
@@ -20,10 +39,25 @@ export const upload = async () => {
 		ContentType: 'image/jpeg',
 	};
 
-	s3.upload(params, function (err: any, data: any) {
+	s3.putObject(params, (err: any, data: any) => {
 		if (err) {
 			throw err;
 		}
 		console.log(`File uploaded successfully. ${data.Location}`);
+	});
+};
+
+export const deleteFile = async () => {
+	const params = {
+		Bucket: 'kirok-user',
+		Key: 'kirok.jpg', // 삭제할 파일의 키
+	};
+
+	s3.deleteObject(params, (err, data) => {
+		if (err) {
+			console.error('Error in file deletion: ', err);
+			throw err;
+		}
+		console.log('File deleted successfully');
 	});
 };
