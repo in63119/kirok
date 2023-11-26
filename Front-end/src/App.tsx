18e690d6a4df1f1a1e3908d1811417085a414451
ddubbu <hello.ddubbu@gmail.com>
2023-11-26 20:56:08 +0800

    chore: 미사용 코드 제거

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -7,7 +7,6 @@ import { PageUrls } from './constants/page-urls';
 // Components
 import Main from './pages/Main';
 import Callback from './pages/Callback';
-import SelectIntro from './pages/SelectIntro';
 import Login from './pages/Login';
 import Institution from './pages/Institution';
 import ParentWelcomePage from './pages/parent/welcome';
@@ -21,8 +20,7 @@ function App() {
 				<Routes>
 					<Route path={PageUrls.INTRO} element={<Main />} />
 					<Route path="/callback" element={<Callback />} />
-					<Route path={PageUrls.SELECT_INTRO} element={<SelectIntro />} />
-					<Route path={PageUrls.LOGIN} element={<Login />} />
+					<Route path={PageUrls.PARENT.KAKAO_LOGIN} element={<Login />} />
 					<Route path={PageUrls.INSTITUTION} element={<Institution />} />
 					<Route path={PageUrls.PARENT.WELCOME} element={<ParentWelcomePage />} />
 					<Route path={PageUrls.PARENT.KID_REGISTER} element={<ParentKidRegisterPage />} />
